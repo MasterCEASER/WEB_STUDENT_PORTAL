16f1292d3900b97179334bf37c436e28d8efa079
momna_aslam <bcsf13m019@pucit.edu.pk>
2015-12-20 17:35:56 +0500

    no message

--- PHP/WEBSITE/contact/contactUs.php
+++ PHP/WEBSITE/contact/contactUs.php
@@ -1,6 +1,6 @@
 <html>
 <head>    
-    <link rel="stylesheet" type="text/css" href="contactUs.css">
+    <link rel="stylesheet" type="text/css" href="../css/newclass.css">
         <link rel="stylesheet" type="text/css" href="../css/headr.css">
         <link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
         <script src="../js/jquery-2.1.4.min.js"></script>
@@ -9,100 +9,78 @@
     
         <?php include('../header.php') ?>;
         <hr style="margin-top:20px;">
-        <div id="content">    
-    <div id="Contact-main">
-    
-        <div id="Contact-header">
-           
+        <div id="content"  style="background-color:lightgray;"> 
+        <div id="newclass-Content1">
+            <div id="newclass-Content1-b">
+                Contact Us                           
+            </div>
         </div>
-        <div id="Contact-content">
-           
-        
-            
-            
-                <div id="Contact-Content1">
-                    <span style="font-size:26px;padding:2px;font-family:sans-serif;"><i><b>Contact Us.</b></i> </span><br><br><br>
-                    <div style="display:table;box-shadow:8px 10px 15px 1px white;margin:10px;margin-bottom:50px;border-color:white;">
-                        <div style="display:table-cell;">
-                                <img src="principle.png"  />
-                        </div>
-                        <div style="display:table-cell;vertical-align:middle;"> 
-                            <centre ><b><i>Principle</i></b></centre><br>
-                                    <b>Name:</b> Dr.Mansoor Sarwar.<br>
-                                    <b>Phn No:</b> 123456<br>
-                                    <b>Email:</b> Principle@pucit.edu.pk.
-                                   
-                        </div> 
-                         
-                            
-                </div>  
-                    <div style="display:table;box-shadow:8px 10px 15px 1px white;margin:10px;margin-bottom:50px;border-color:white;">
-                        <div style="display:table-cell;">
-                                <img src="sirasim.gif"  />
-                        </div>
-                        <div style="display:table-cell;vertical-align:middle;"> 
-                            <centre><b><i>Examination Branch.</i></b></centre>
-                            <br>
-                            <b>Head Name:</b>Asim Rasool.<br>
-                            <b>Ph No:</b> 111-923-923 ext-148.<br>
-                            <b>Email:</b>examination@pucit.edu.pk.<br><br>
-            
-                                   
-                            
-                </div>
+        <br><hr><br>
+        <div id="classroomS-Content2 ">
+            <div style="background-color:lightgray;">
+                <div class="class_table">
+                    <div style="display:table-cell;">
+                        <img class="class_img" src="../image/principle.png" style="min-width: 160px;max-width: 160px;">
                     </div>
-                         <div style="display:table;box-shadow:8px 10px 15px 1px white;margin:10px;margin-bottom:50px;border-color:white;">
-                        <div style="display:table-cell;">
-                                <img src="siramir.gif"  />
-                        </div>
-                        <div style="display:table-cell;vertical-align:middle;"> 
-                            <centre><b><i>Program Cordinator.</i></b></centre>
-                            <br>
-                             <b>Coordinator Name:</b>Amir Latif.<br>
-                             <b>Ph No:</b> 111-923-923 ext-148.<br>
-                             <b>Email:</b>examination@pucit.edu.pk.<br><br>
-                            
-            
-                                   
-                            
-                </div>
+                    <div style="display:table-cell;vertical-align:middle;padding-left: 60px;">
+                            <h2 style="text-align: center;">Principle</h2>       
+                        <ul>
+                            <li><b>Name:</b> Dr.Mansoor Sarwar.</li>
+                            <li><b>Phn No:</b> 123456</li>
+                            <li><b>Email:</b> Principle@pucit.edu.pk.</li>
+                        </ul>
                     </div>
-                      <div style="display:table;box-shadow:8px 10px 15px 1px white;margin:10px;margin-bottom:50px;border-color:white;">
-                        
-                        <div style="display:table-cell;vertical-align:middle;"> 
-                            <centre><b><i>Admission Office.</i></b></centre>
-                            <br>
-                                <b>Ph No:</b> 111-923-923 ext-149.<br>
-                                <b>Email:</b>admission@pucit.edu.pk.<br><br>
-                 
-                            
-                        
-                                   
-                            
-                </div>   
-                           </div>
-                      <div style="display:table;box-shadow:8px 10px 15px 1px white;margin:10px;margin-bottom:50px;border-color:white;">
-                        
-                        <div style="display:table-cell;vertical-align:middle;"> 
-                            <centre><b><i>Address</i></b></centre>
-                            <br>
-                                &nbsp;&nbsp;&nbsp;Punjab University college of information technology,<br>Shahrahe Quaid-e-Azam                                          Allama IqbalCampus <br>(Old Campus) Lahore, Pakistan.
-                 
-                            
-                        
-                                   
-                            
-                </div>     
-                    </div>
-                     
                 </div>
-            
-               
-            
-            
-            
-             
-        
+                <div class="class_table">
+                    <div style="display:table-cell;">
+                        <img class="class_img" src="../image/sirasim.gif" style="min-width: 160px;max-width: 160px;">
+                    </div>
+                    <div style="display:table-cell;vertical-align:middle;padding-left: 60px;">
+                            <h2 style="text-align: center;">Examination Branch</h2>       
+                        <ul>
+                            <li><b>Name:</b> Asim Rasool.</li>
+                            <li><b>Phn No:</b> 123456</li>
+                            <li><b>Email:</b> examination@pucit.edu.pk.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="class_table">
+                    <div style="display:table-cell;">
+                        <img class="class_img" src="../image/siramir.gif" style="min-width: 160px;max-width: 160px;">
+                    </div>
+                    <div style="display:table-cell;vertical-align:middle;padding-left: 60px;">
+                            <h2 style="text-align: center;">Program Cordinator.</h2>       
+                        <ul>
+                            <li><b>Name:</b>Amir Latif.</li>
+                            <li><b>Phn No:</b>  111-923-923 ext-148.</li>
+                            <li><b>Email:</b> amir@pucit.edu.pk.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="class_table">
+                    <div style="display:table-cell;vertical-align:middle;">
+                            <h2 style="text-align: center;">Admission Office.</h2>       
+                        <ul style="
+    margin-left: 25%;
+">
+                            <li><b>Phn No:</b>  111-923-923 ext-149.</li>
+                            <li><b>Email:</b> admission@pucit.edu.pk.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="class_table">
+                    <div style="display:table-cell;vertical-align:middle;">
+                        <h2 style="text-align: center;">Address</h2>       
+                        <ul style="margin-left: 8%;margin-right: 8%;width: auto;">
+                            <li style="width: 450px;">
+                                Punjab University college of information technology,
+                                Shahrahe Quaid-e-Azam Allama Iqbal Campus 
+                                (Old Campus) Lahore, Pakistan.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     
     </div>
